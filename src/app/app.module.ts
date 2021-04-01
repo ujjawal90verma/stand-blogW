77e93670f635dffff3468382d5b3f5bc48c165f9
Ujjawal Verma <ujjawal90verma@gmail.com>
2021-04-01 10:48:14 +0530

    Made some changes in folder structure and added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { HeaderComponent } from './modules/core/header/header.component';
 import { FooterComponent } from './modules/core/footer/footer.component';
 import { CarouselComponent } from './modules/home/components/carousel/carousel.component';
 import { CarouselItemComponent } from './modules/home/components/carousel-item/carousel-item.component';
+import { HomeComponent } from './modules/home/home/home.component';
+import { AboutUsComponent } from './modules/about-us/about-us/about-us.component';
+import { BlogEntriesComponent } from './modules/blog-entries/blog-entries/blog-entries.component';
+import { ContactUsComponent } from './modules/contact-us/contact-us/contact-us.component';
+import { CoreComponent } from './modules/core/core/core.component';
+import { PostDetailsComponent } from './modules/post-details/post-details/post-details.component';
+import { SharedComponent } from './modules/shared/shared/shared.component';
 
 
 
@@ -17,6 +24,13 @@ import { CarouselItemComponent } from './modules/home/components/carousel-item/c
     FooterComponent,
     CarouselComponent,
     CarouselItemComponent,
+    HomeComponent,
+    AboutUsComponent,
+    BlogEntriesComponent,
+    ContactUsComponent,
+    CoreComponent,
+    PostDetailsComponent,
+    SharedComponent,
     
     
   ],
